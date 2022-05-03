56384796a49e97e472fe483399ebf3159ab1dd99
emmamarks27 <100089874+emmamarks27@users.noreply.github.com>
2022-05-03 17:45:15 +0100

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <h1>Hello from dictionary app</h1>
+      <button className="btn-secondary">Hello there!</button>
+    </div>
+  );
+}
